59006704ca7a82ad2eaa39751681542f2bb47b51
TheWOLDP <79740369+TheWOLDP@users.noreply.github.com>
2021-06-19 02:58:46 -0700

    update UI

--- Calculator/ContentView.swift
+++ Calculator/ContentView.swift
@@ -8,10 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    let buttons = [
+        ["7","8","9","X"],
+        ["4","5","6","-"],
+        ["1","2","3","+"],
+        ["0",".",".","="]
+    ]
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack (alignment:.bottom){
+      
+            Color.black
+                .edgesIgnoringSafeArea(.all)
+            VStack(spacing:12){
+                HStack{
+                    Spacer()
+                    Text("11456")
+                        .foregroundColor(.white)
+                        .font(.system(size: 64))
+                }.padding()
+                
+                ForEach(buttons,id: \.self){row in
+                    HStack (spacing: 12){
+                        ForEach(row, id: \.self){button in
+                            Text(button)
+                                .font(.system(size: 32))
+                                .frame(width: self.buttonWidth(), height: self.buttonWidth())
+                                .foregroundColor(.white)
+                                .background(Color.yellow)
+                                .cornerRadius(40)
+                        }
+                    }
+                }
+            }
+        }.padding(.bottom)
+        .edgesIgnoringSafeArea(.all)
+
     }
+    
+    func buttonWidth() -> CGFloat{
+        return (UIScreen.main.bounds.width - 5 * 12) / 4
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
